4e34af0c226104cc135eb37d81e53e4962043984
Sanoop-PR <sanooppr60@gmail.com>
2023-12-10 13:08:17 +0530

    mern-todo

--- frontend/src/firebase.js
+++ frontend/src/firebase.js
@@ -5,12 +5,12 @@ import { initializeApp } from 'firebase/app';
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyBEtRLBqaFTWma3ax1G8MdYJpV7m8w10MA",
-  authDomain: "mern-auth-491b4.firebaseapp.com",
-  projectId: "mern-auth-491b4",
-  storageBucket: "mern-auth-491b4.appspot.com",
-  messagingSenderId: "261585039021",
-  appId: "1:261585039021:web:128c8458a8a529a4756024"
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: ""
 };
 
 // Initialize Firebase
